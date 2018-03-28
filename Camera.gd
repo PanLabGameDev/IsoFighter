5b31e9f70cf2bdff5b182326df459c98c80318bc
Christian Winter <christian.winter1990@gmail.com>
2018-03-28 23:24:58 +0200

    Clean up

--- Camera.gd
+++ Camera.gd
@@ -1,13 +1,10 @@
 extends Camera
 
-# class member variables go here, for example:
 export var distance = 10.0
 export var height = 5.0
 export var target_path = "/root/Game/Player"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	set_physics_process(true)
 
 	set_as_toplevel(true)
